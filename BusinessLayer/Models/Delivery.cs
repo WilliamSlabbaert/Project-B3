74922a00d3ef266e9b1f073fc6a6e0545564658f
Tim De Smet <tim.de.smet@hotmail.be>
2021-01-11 20:05:39 +0100

    Update database

---
+++ BusinessLayer/Models/Delivery.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Models
+{
+    public class Delivery
+    {
+        #region Attributes
+        public int ID { get; private set; }
+        public string Supplier { get; private set; }
+        #endregion
+
+        #region Constructor 
+        public Publisher(int id, string name)
+        {
+            this.ID = id;
+            SetName(name);
+        }
+        public Publisher(string name)
+        {
+            SetName(name);
+        }
+        #endregion
+
+        #region Methods 
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new InvalidNameExcetion();
+            this.Name = name;
+        }
+        #endregion
+
+        #region Override
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType())) return false;
+            Publisher p = (Publisher)obj;
+            if (this.ID == p.ID) return true;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.ID, this.Name);
+        }
+        #endregion
+
+        #region Exceptions
+        public class InvalidNameExcetion : Exception
+        {
+            public InvalidNameExcetion() : base(String.Format("The publishers name cannot be empty")) { }
+        }
+        #endregion
+    }
+}
--- DataLayer/Database/database.sql
+++ DataLayer/Database/database.sql
@@ -17,13 +17,14 @@ GO
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Comicstrips' AND xtype='U')
     CREATE TABLE [dbo].[Comicstrips] (
-    [Id]     INT          IDENTITY (1, 1) NOT NULL,
-    [Titel]  VARCHAR (50) NULL,
-    [Serie_Id] INT NULL,
-    [Number] INT          NULL,
-    [Publisher_Id] INT NULL, 
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
+        [Id]           INT          IDENTITY (1, 1) NOT NULL,
+        [Title]        VARCHAR (50) NULL,
+        [Serie_Id]     INT          NULL,
+        [Number]       INT          NULL,
+        [Publisher_Id] INT          NULL,
+        [Stock] INT NOT NULL DEFAULT 0, 
+        PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
 GO
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ComicstripAuthors' AND xtype='U')
@@ -40,4 +41,59 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ComicstripSeries' AND xtype=
         [Name] VARCHAR(50) NULL
         PRIMARY KEY CLUSTERED ([Id] ASC)
     )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ComicstripBundles' AND xtype='U')
+    CREATE TABLE [dbo].[ComicstripBundles] (
+        [Id]           INT          IDENTITY (1, 1) NOT NULL,
+        [Title]        VARCHAR (50) NULL,
+        [Publisher_Id] INT          NULL,
+        PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ComicstripBundleComicstrips' AND xtype='U')
+    CREATE TABLE [dbo].[ComicstripBundleComicstrips] (
+        [ComicstripBundle_Id] INT NOT NULL,
+        [Comicstrip_Id]       INT NOT NULL
+    );
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Deliveries' AND xtype='U')
+    CREATE TABLE [dbo].[Deliveries]
+    (
+	    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+        [Supplier] VARCHAR(50) NULL, 
+        [Date] DATETIME NULL
+    );
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='DeliveryItems' AND xtype='U')
+    CREATE TABLE [dbo].[DeliveryItems]
+    (
+	    [Id] INT NOT NULL PRIMARY KEY, 
+        [Comicstrip_Id] INT NOT NULL, 
+        [Quantity] INT NOT NULL DEFAULT 1
+    );
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Orders' AND xtype='U')
+    CREATE TABLE [dbo].[Orders]
+    (
+	    [Id] INT NOT NULL PRIMARY KEY, 
+        [FirstName] VARCHAR(50) NULL, 
+        [LastName] VARCHAR(50) NULL, 
+	    [Email] VARCHAR(100 ) NULL,
+	    [Phone] VARCHAR(50) NULL, 
+        [Date] DATETIME NULL
+    )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='OrderItems' AND xtype='U')
+    CREATE TABLE [dbo].[OrderItems] (
+        [Id]            INT NOT NULL,
+        [Comicstrip_Id] INT NOT NULL,
+        [Quantity]      INT DEFAULT ((1)) NOT NULL,
+        PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
 GO
